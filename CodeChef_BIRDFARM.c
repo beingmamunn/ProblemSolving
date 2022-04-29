c241bd8a092c3e954b1c88743fb03ae297d43403
Mamun billah <beingmamun@outlook.com>
2022-04-30 03:13:37 +0600

    Add files via upload

---
+++ CodeChef_BIRDFARM.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+     int t, x, y, z;
+     scanf("%d", &t);
+     while(t--)
+     {
+          scanf("%d %d %d", &x, &y, &z);
+          if((z%x)==0 && (z%y)==0)
+               printf("ANY\n");
+          else if((z%x)==0 && (z%y)!=0)
+               printf("CHICKEN\n");
+          else if((z%x)!=0 && (z%y)==0)
+               printf("DUCK\n");
+          else if((z%x)!=0 && (z%y)!=0)
+               printf("NONE\n");
+     }
+}
